6ac326d6d041e5182b9b04a5a11ab4866da282aa
Serhii Myshko <sergeiown@gmail.com>
2024-02-01 05:31:44 +0200

    Change command line messages to English for compatibility

--- autorun.bat
+++ autorun.bat
@@ -1,12 +1,12 @@
 @echo off
 setlocal enabledelayedexpansion
 
-echo Введіть повний шлях до файлу start_hidden.bat:
+echo Enter the full path to the start_hidden.bat file:
 set /p bat_path=
 
 :: Перевірка чи файл існує
 if not exist "!bat_path!" (
-    echo Файл не знайдено. Перевірте шлях та спробуйте ще раз.
+    echo File not found. Please check the path and try again.
     timeout /t 2 /nobreak > nul
     goto :eof
 )
@@ -15,7 +15,7 @@ if not exist "!bat_path!" (
 set "startup_folder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 copy "!bat_path!" "!startup_folder!"
 
-echo start_hidden.bat успішно доданий до автозапуску!
+echo start_hidden.bat successfully added to startup!
 timeout /t 2 /nobreak > nul
 
 :end
