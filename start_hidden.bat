ef9fb83f6ff11629f98b8820c9842dab621a87f6
Serhii Myshko <sergeiown@gmail.com>
2024-02-01 05:37:28 +0200

    add additional information messages

--- start_hidden.bat
+++ start_hidden.bat
@@ -4,6 +4,7 @@ set "VBSFile=%temp%\invisible.vbs"
 echo CreateObject("Wscript.Shell").Run "powershell -WindowStyle Hidden -Command ""node index.js""", 0 > %VBSFile%
 start /b %VBSFile%
 
+echo Alert update server is successfully started.
 timeout /t 1 /nobreak > nul
 
 del %VBSFile%
