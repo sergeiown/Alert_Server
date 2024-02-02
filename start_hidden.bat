021f4283963bf6cc12b2fc50f4241c8633c7b076
Serhii Myshko <sergeiown@gmail.com>
2024-02-02 13:10:08 +0200

    add a safeguard against running multiple instances of a process

--- start_hidden.bat
+++ start_hidden.bat
@@ -1,6 +1,15 @@
 @echo off
-set "VBSFile=%temp%\invisible.vbs"
 
+set "VBSFile=%temp%\invisible.vbs"
+set "SessionFile=session.tmp"
+
+:: Зчитування PID з файлу session.tmp
+for /f %%i in (%SessionFile%) do set "NodePID=%%i"
+
+:: Завершення процесу із вказаним PID
+taskkill /f /pid %NodePID% >nul 2>nul
+
+:: Запуск нового процесу у фоновому режимі
 echo CreateObject("Wscript.Shell").Run "powershell -WindowStyle Hidden -Command ""node index.js""", 0 > %VBSFile%
 start /b %VBSFile%
 
@@ -8,3 +17,8 @@ echo Alert update server is successfully started.
 timeout /t 1 /nobreak > nul
 
 del %VBSFile%
+
+:: Запис нового PID у файл session.tmp
+for /f "tokens=2 delims=," %%i in ('tasklist /nh /fi "imagename eq node.exe" /fo csv ^| findstr /i "node.exe"') do (
+    echo %%i > %SessionFile%
+)
--- start_visible.bat
+++ start_visible.bat
@@ -1,6 +1,22 @@
 @echo off
 
-start /b "" powershell -WindowStyle Hidden -Command "node index.js"
+set "SessionFile=session.tmp"
 
+:: Зчитування PID з файлу session.tmp
+for /f %%i in (%SessionFile%) do set "NodePID=%%i"
+
+:: Завершення процесу із вказаним PID
+taskkill /f /pid %NodePID% >nul 2>nul
+
+:: Запуск нового процесу
+start /b "" powershell -WindowStyle Hidden -Command "node index.js"
 echo Alert update server is successfully started.
-echo.
+echo Date,Time,Event
+timeout /t 1 /nobreak >nul
+
+:: Запис нового PID у файл session.tmp
+for /f "tokens=2 delims=," %%i in ('tasklist /nh /fi "imagename eq node.exe" /fo csv ^| findstr /i "node.exe"') do (
+    echo %%i > %SessionFile%
+)
+
+
