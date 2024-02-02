2940c9203a79466ed679b94de3dbd5cdd0a3296b
Serhii Myshko <sergeiown@gmail.com>
2024-02-02 11:05:57 +0200

    change log format

--- modules/logger.js
+++ modules/logger.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const logFilePath = path.join(__dirname, '../log.txt');
 
-const logError = (errorMessage) => {
+const logError = (eventMessage) => {
     const currentDateTime = new Date()
         .toLocaleString('UA', {
             year: 'numeric',
@@ -15,7 +15,20 @@ const logError = (errorMessage) => {
             second: '2-digit',
         })
         .replace(',', '');
-    const logMessage = `${currentDateTime} ${errorMessage}\n`;
+
+    const maxMessageLength = 100;
+    const separator = '.';
+
+    if (eventMessage.length > maxMessageLength - currentDateTime.length - separator.length) {
+        eventMessage = eventMessage.slice(0, maxMessageLength - currentDateTime.length - separator.length);
+    }
+
+    const eventMessagePadded = eventMessage.padStart(
+        maxMessageLength - currentDateTime.length - separator.length,
+        separator
+    );
+
+    const logMessage = `[${currentDateTime}]${eventMessagePadded}\n`;
 
     fs.appendFileSync(logFilePath, logMessage, 'utf-8');
     log(logMessage);
