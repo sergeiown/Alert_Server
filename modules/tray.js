ce32b79f2102866edc503226c69a8daceeec7516
Serhii Myshko <sergeiown@gmail.com>
2024-02-03 18:11:06 +0200

    add information about the app tray menu

--- modules/tray.js
+++ modules/tray.js
@@ -9,6 +9,7 @@ function createTrayIcon() {
     let isAlertActive = false;
 
     Tray.create(function (tray) {
+        // Пункт меню 'Поточні тривоги'
         let main = tray.item('Поточні тривоги');
 
         let alertsItem = tray.item('alerts.in.ua', () => {
@@ -21,30 +22,57 @@ function createTrayIcon() {
         });
         main.add(alertsItem);
 
-        let logView = tray.item('Перегляд журналу');
+        // Пункт меню 'Інформація'
+        let logView = tray.item('Інформація');
 
-        let logItem = tray.item('log.csv', () => {
+        // Підпункт 'Інформація' => 'Перегляд журналу'
+        let logItem = tray.item('Перегляд журналу', () => {
             exec('start log.csv', (error, stdout, stderr) => {
                 if (error) {
-                    logEvent(`Помилка відкриття журналу: ${error.message}`);
+                    logEvent(`Error opening the log file: ${error.message}`);
                     return;
                 }
             });
         });
         logView.add(logItem);
 
+        // Підпункт 'Інформація' => 'Про програму'
+        let about = tray.item('Про програму', () => {
+            const aboutMessage = `Локальний сервер оновлення тривог - це Node.js сервер, який із завданою періодичністю отримує дані про тривоги з alerts.in.ua API та зберігає їх дані з подальшою обробкою і виводом повідомлення про початок та закінчення тривоги для зазначеного регіону України.                                                                                                                                      Copyright (c) 2024 Serhii I. Myshko`;
+
+            const vbsPath = path.join(__dirname, 'msgbox.vbs');
+
+            fs.writeFileSync(
+                vbsPath,
+                `MsgBox "${aboutMessage.replace(/\r?\n/g, ' ')}", 64, "Про програму"`,
+                'utf-16le'
+            );
+
+            exec(`start wscript.exe "${vbsPath}"`, (error, stdout, stderr) => {
+                if (error) {
+                    logEvent(`Error opening the message window: ${error.message}`);
+                    return;
+                }
+
+                fs.unlinkSync(vbsPath);
+            });
+        });
+
+        logView.add(about);
+
+        // Пункт меню 'Вихід'
         let quit = tray.item('Вихід', () => {
-            logEvent(`Сервер зупинений користувачем`);
+            logEvent(`The server is stopped by the user`);
             tray.kill();
             process.exit();
         });
 
         tray.setMenu(main, logView, quit);
 
+        // Оновлення трея у відповідності до наявності тривоги
         function checkAlertStatus() {
             const tempFilePath = path.join(process.env.TEMP, 'alert_active.tmp');
 
-            // Змінюємо трей у відповідності до наявності тривоги
             fs.access(tempFilePath, fs.constants.F_OK, (err) => {
                 if (err) {
                     if (isAlertActive) {
@@ -64,7 +92,6 @@ function createTrayIcon() {
             });
         }
 
-        // Встановлюємо інтервал оновлення трея
         setInterval(checkAlertStatus, 5000);
 
         tray.setTitle('Alert server');
