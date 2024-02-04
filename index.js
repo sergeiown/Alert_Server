b2cdcc3ce5ac183498148d198f85d7dd12a0d696
Serhii Myshko <sergeiown@gmail.com>
2024-02-04 11:23:20 +0200

    rename tray module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { logEvent } = require('./modules/logger');
 const { fetchDataAndSaveToFile } = require('./modules/api');
 const { showNotification } = require('./modules/notification');
-const { createTrayIcon } = require('./modules/tray');
+const { createTrayIcon } = require('./modules/trayIcon');
 
 logEvent(`Start of the server`);
 
