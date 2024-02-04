9e6427b173cbd4b1a8c6796f193680681463c9c8
Serhii Myshko <sergeiown@gmail.com>
2024-02-04 19:53:09 +0200

    fix startup activator script with workingDirectory

--- startup_activator.bat
+++ startup_activator.bat
@@ -3,9 +3,10 @@ set shortcutName=start_alertserver_hidden
 set targetPath=%CD%\start_alertserver_hidden.bat
 set shortcutPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\%shortcutName%.lnk
 set iconPath=%SystemRoot%\System32\SHELL32.dll,77
+set workingDirectory=%CD%
 
 if exist "%shortcutPath%" (
     del "%shortcutPath%"
 ) else (
-    powershell -Command "$WScript=New-Object -ComObject WScript.Shell; $Shortcut=$WScript.CreateShortcut('%shortcutPath%'); $Shortcut.TargetPath='%targetPath%'; $Shortcut.IconLocation='%iconPath%'; $Shortcut.Save()"
+    powershell -Command "$WScript=New-Object -ComObject WScript.Shell; $Shortcut=$WScript.CreateShortcut('%shortcutPath%'); $Shortcut.TargetPath='%targetPath%'; $Shortcut.IconLocation='%iconPath%'; $Shortcut.WorkingDirectory='%workingDirectory%'; $Shortcut.Save()"
 )
