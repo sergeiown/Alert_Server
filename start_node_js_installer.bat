ab430c164cc9f80b6b068cfd588cb8cf24520404
Serhii Myshko <sergeiown@gmail.com>
2024-02-06 00:19:01 +0200

    change the method to winget

--- start_node_js_installer.bat
+++ start_node_js_installer.bat
@@ -5,27 +5,8 @@ where node > nul 2>nul
 
 IF %ERRORLEVEL% NEQ 0 (
     
-    :: Завантаження HTML, видобування імені файлу та викачування
-    powershell -Command "& { Invoke-WebRequest -Uri 'https://nodejs.org/dist/latest/' -OutFile '%temp%\temp.html' }"
-
-    setlocal enabledelayedexpansion
-
-    set "filename="
-    for /f "tokens=2 delims=<>" %%x in ('type %temp%\temp.html ^| find "x64.msi"') do set "filename=%%x"
-       
-    echo Downloading !filename!
-    powershell -Command "& { Invoke-WebRequest -Uri 'https://nodejs.org/dist/latest/!filename!' -OutFile '%temp%\x64.msi' }"
+    winget install OpenJS.NodeJS
     
-    endlocal
-   
-    :: Інсталяція
-    echo Installing Node.js...
-    start /wait msiexec /i %temp%\x64.msi /qn /qb! /norestart
-
-    :: Прибирання зайвого
-    del %temp%\temp.html /Q 2>nul
-    del %temp%\x64.msi /Q 2>nul
-
     timeout /nobreak /t 1 >nul
 
 ) ELSE (
