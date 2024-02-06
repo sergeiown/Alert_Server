76b4f2a32dfb3bc700273f4cf77cbfe0a0d73744
Serhii Myshko <sergeiown@gmail.com>
2024-02-06 17:43:31 +0200

    improve start_menu_shortcut

--- start_menu_shortcut.bat
+++ start_menu_shortcut.bat
@@ -9,7 +9,7 @@ set iconPath=%SystemRoot%\System32\SHELL32.dll,77
 set workingDirectory=%CD%
 
 if exist "%shortcutPath%" (
-    exit /b 0
-) else (
-    powershell -Command "$WScript=New-Object -ComObject WScript.Shell; $Shortcut=$WScript.CreateShortcut('%shortcutPath%'); $Shortcut.TargetPath='%targetPath%'; $Shortcut.IconLocation='%iconPath%'; $Shortcut.WorkingDirectory='%workingDirectory%'; $Shortcut.Save()"
+    del "%shortcutPath%"
 )
+
+powershell -Command "$WScript=New-Object -ComObject WScript.Shell; $Shortcut=$WScript.CreateShortcut('%shortcutPath%'); $Shortcut.TargetPath='%targetPath%'; $Shortcut.IconLocation='%iconPath%'; $Shortcut.WorkingDirectory='%workingDirectory%'; $Shortcut.Save()"
