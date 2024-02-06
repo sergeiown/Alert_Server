289af7d86758fb12445bb06569ac1e3ddcd4b33f
Serhii Myshko <sergeiown@gmail.com>
2024-02-06 12:01:45 +0200

    improve tray menu

--- modules/trayMenu.js
+++ modules/trayMenu.js
@@ -33,15 +33,8 @@ function createUpdateDateTimeMenu(tray) {
         return formattedDate;
     }
 
-    function updateLastUpdateDateTime() {
-        const newLastUpdate = getLastUpdateDateTime();
-        lastUpdateDateTime = tray.item(`Остання зміна стану: ${newLastUpdate}`, { disabled: true });
-    }
-
     const lastUpdate = getLastUpdateDateTime();
     let lastUpdateDateTime = tray.item(`Остання зміна стану: ${lastUpdate}`, { disabled: true });
-
-    setInterval(updateLastUpdateDateTime, 10000);
 
     return lastUpdateDateTime;
 }
