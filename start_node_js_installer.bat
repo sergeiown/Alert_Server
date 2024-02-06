efc1e73de200f96bac74bde8116d49795c7040df
Serhii Myshko <sergeiown@gmail.com>
2024-02-06 11:04:51 +0200

    update start server script

--- start_node_js_installer.bat
+++ start_node_js_installer.bat
@@ -3,13 +3,13 @@
 :: Перевірка наявності Node.js, завантаження та встановлення за необхідності
 where node > nul 2>nul
 
-IF %ERRORLEVEL% NEQ 0 (
+if %ERRORLEVEL% neq 0 (
     
     winget install OpenJS.NodeJS
     
     echo. & timeout /nobreak /t 2 >nul
 
-) ELSE (
+) else (
     echo Node.js is present in the system.
 
     call start_alertserver_hidden.bat
