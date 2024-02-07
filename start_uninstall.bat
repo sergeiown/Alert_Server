893e26082ac73a4f6bb2e21a2aa0fee6ffdbe043
Serhii Myshko <sergeiown@gmail.com>
2024-02-07 18:53:33 +0200

    update uninstall

--- start_uninstall.bat
+++ start_uninstall.bat
@@ -7,12 +7,8 @@ taskkill /f /im "node.exe" >nul 2>nul
 
 timeout /t 1 /nobreak >nul
 
-rmdir /s /q "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Alert server\"
-
 del /q "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Alert server.lnk"
 
+rmdir /s /q "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Alert server\"
+
 rmdir /s /q "%userprofile%\Documents\Alert server"
-
-echo Alert server uninstallation was successful.
-
-timeout /t 1 /nobreak >nul
