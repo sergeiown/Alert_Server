b709667bf920d8c03fd0b453ea55a100ddb7379f
Serhii Myshko <sergeiown@gmail.com>
2024-02-08 02:25:19 +0200

    update uninstall

--- start_uninstall.bat
+++ start_uninstall.bat
@@ -3,6 +3,13 @@
 
 @echo off
 
+:: Запит на підвищення привілеїв до адміністратора
+whoami /groups | find "S-1-16-12288" > nul
+    if %errorlevel% neq 0 (
+        powershell -Command "Start-Process '%0' -Verb RunAs"
+        exit /b
+    )
+
 taskkill /f /im "node.exe" >nul 2>nul
 
 timeout /t 1 /nobreak >nul
