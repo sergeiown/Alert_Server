89421e4826314d30a89f2e6132feb7794db63582
Serhii I. Myshko <112722061+sergeiown@users.noreply.github.com>
2024-02-09 21:46:13 +0200

    Update start_node_js_installer.bat

--- start_node_js_installer.bat
+++ start_node_js_installer.bat
@@ -7,8 +7,8 @@
 where node > nul 2>nul
 
 if %ERRORLEVEL% neq 0 (
-    powershell -Command "Start-Process 'winget install OpenJS.NodeJS --accept-source-agreements' -Verb RunAs -Wait"
-
+    powershell -Command "Start-Process 'winget' -ArgumentList 'install OpenJS.NodeJS --accept-source-agreements' -Verb RunAs -Wait"
+   
     echo NodeJS is successfully installed.
     
     echo. & timeout /nobreak /t 2 >nul
