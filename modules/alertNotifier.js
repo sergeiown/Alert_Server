4e99926e0311cd2b8ee4935d1d2e37b4b79cf95b
Serhii Myshko <sergeiown@gmail.com>
2024-08-13 16:17:47 +0300

    change the file name for the uninstaller to work correctly

--- modules/alertNotifier.js
+++ modules/alertNotifier.js
@@ -15,7 +15,7 @@ const { logEvent } = require('./logger');
 
 const displayedAlerts = new Map();
 const tempFilePath = path.join(process.env.TEMP, 'alert_active.tmp');
-const displayedAlertsFilePath = path.join(process.env.TEMP, 'alerts_displayed.json');
+const displayedAlertsFilePath = path.join(process.env.TEMP, 'alert_displayed.json');
 
 if (fs.existsSync(tempFilePath)) {
     fs.unlinkSync(tempFilePath);
