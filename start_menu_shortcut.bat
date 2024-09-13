2e134c4edb8eb48a0ee83e770eaca447d701fb9d
Serhii Myshko <sergeiown@gmail.com>
2024-09-13 14:08:21 +0300

    add a shortcut to launch alert server with console

--- start_menu_shortcut.bat
+++ start_menu_shortcut.bat
@@ -4,8 +4,11 @@
 @echo off
 set shortcutName=Alert Server
 set shortcutTargetDescription=Run Alert Server in background
+set shortcutAltName=Alert server with console
+set shortcutAltTargetDescription=Run Alert Server with console
 set shortcutUninstallDescription=Uninstall Alert Server
 set targetPath=%CD%\start_alertserver_hidden.bat
+set targetAltPath=%CD%\start_alertserver_visible.bat
 set uninstallPath=%CD%\start_uninstall.bat
 set folderPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\%shortcutName%
 set iconPath=%SystemRoot%\System32\SHELL32.dll,77
@@ -18,6 +21,8 @@ if exist "%folderPath%" (
 mkdir "%folderPath%"
 
 powershell -Command "$WScript=New-Object -ComObject WScript.Shell; $Shortcut=$WScript.CreateShortcut('%folderPath%\%shortcutName%.lnk'); $Shortcut.TargetPath='%targetPath%'; $Shortcut.IconLocation='%iconPath%'; $Shortcut.WorkingDirectory='%workingDirectory%'; $Shortcut.Description='%shortcutTargetDescription%'; $Shortcut.WindowStyle=7; $Shortcut.Save()"
+
+powershell -Command "$WScript=New-Object -ComObject WScript.Shell; $Shortcut=$WScript.CreateShortcut('%folderPath%\%shortcutAltName%.lnk'); $Shortcut.TargetPath='%targetAltPath%'; $Shortcut.IconLocation='%iconPath%'; $Shortcut.WorkingDirectory='%workingDirectory%'; $Shortcut.Description='%shortcutAltTargetDescription%'; $Shortcut.WindowStyle=1; $Shortcut.Save()"
 
 powershell -Command "$WScript=New-Object -ComObject WScript.Shell; $Shortcut=$WScript.CreateShortcut('%folderPath%\Uninstall.lnk'); $Shortcut.TargetPath='%uninstallPath%'; $Shortcut.IconLocation='%iconPath%'; $Shortcut.WorkingDirectory='%workingDirectory%'; $Shortcut.Save(); $Shortcut = $WScript.CreateShortcut('%folderPath%\Uninstall.lnk'); $Shortcut.TargetPath='%uninstallPath%'; $Shortcut.WorkingDirectory='%workingDirectory%'; $Shortcut.Arguments = '-Verb RunAs'; $Shortcut.Description='%shortcutUninstallDescription%'; $Shortcut.WindowStyle=7; $Shortcut.Save()"
 
