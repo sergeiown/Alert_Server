ada07f4263469d65a05bab9d6a3c57ea4064c56d
Serhii Myshko <sergeiown@gmail.com>
2024-09-15 21:49:29 +0300

    improve the naming of program menu shortcuts

--- start_menu_shortcut.bat
+++ start_menu_shortcut.bat
@@ -2,15 +2,18 @@
 :: https://github.com/sergeiown/Alert_Server/blob/main/LICENSE
 
 @echo off
+
+set folderName=Alert Server
+set folderPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\%folderName%
 set shortcutName=Alert Server tray
-set shortcutTargetDescription=Run Alert Server in background
-set shortcutAltName=Alert Server tray and console
-set shortcutAltTargetDescription=Run Alert Server with console
-set shortcutUninstallDescription=Uninstall Alert Server
+set shortcutTargetDescription=Run Alert Server minimized to the tray
 set targetPath=%CD%\start_alertserver_hidden.bat
+set shortcutAltName=Alert Server tray and console
+set shortcutAltTargetDescription=Run Alert Server minimized to the tray with access to the console
 set targetAltPath=%CD%\start_alertserver_visible.bat
+set shortcutUninstallName=Uninstall Alert Server
+set shortcutUninstallDescription=Uninstall Alert Server from the system
 set uninstallPath=%CD%\start_uninstall.bat
-set folderPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\%shortcutName%
 set iconPath=%SystemRoot%\System32\SHELL32.dll,77
 set workingDirectory=%CD%
 
@@ -40,7 +43,7 @@ if errorlevel 1 (
     exit /b 1
 )
 
-powershell -Command "$WScript=New-Object -ComObject WScript.Shell; $Shortcut=$WScript.CreateShortcut('%folderPath%\Uninstall.lnk'); $Shortcut.TargetPath='%uninstallPath%'; $Shortcut.IconLocation='%iconPath%'; $Shortcut.WorkingDirectory='%workingDirectory%'; $Shortcut.Save(); $Shortcut = $WScript.CreateShortcut('%folderPath%\Uninstall.lnk'); $Shortcut.TargetPath='%uninstallPath%'; $Shortcut.WorkingDirectory='%workingDirectory%'; $Shortcut.Arguments = '-Verb RunAs'; $Shortcut.Description='%shortcutUninstallDescription%'; $Shortcut.WindowStyle=7; $Shortcut.Save()"
+powershell -Command "$WScript=New-Object -ComObject WScript.Shell; $Shortcut=$WScript.CreateShortcut('%folderPath%\%shortcutUninstallName%.lnk'); $Shortcut.TargetPath='%uninstallPath%'; $Shortcut.IconLocation='%iconPath%'; $Shortcut.WorkingDirectory='%workingDirectory%'; $Shortcut.Save(); $Shortcut = $WScript.CreateShortcut('%folderPath%\Uninstall.lnk'); $Shortcut.TargetPath='%uninstallPath%'; $Shortcut.WorkingDirectory='%workingDirectory%'; $Shortcut.Arguments = '-Verb RunAs'; $Shortcut.Description='%shortcutUninstallDescription%'; $Shortcut.WindowStyle=7; $Shortcut.Save()"
 if errorlevel 1 (
     echo Failed to create shortcut "%folderPath%\Uninstall.lnk"
     exit /b 1
