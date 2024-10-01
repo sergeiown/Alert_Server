a272e61aafcf059f547d361b6e4671560c01270c
sergeiown <sergeiown@gmail.com>
2024-10-01 21:49:36 +0300

    Add a check for standard directories where npm can be installed if it is not found in the PATH

--- start_dependencies_installer.bat
+++ start_dependencies_installer.bat
@@ -7,18 +7,30 @@ title Dependencies installation
 for /f "delims=" %%i in ('where npm') do set "npmPath=%%i"
 
 if not defined npmPath (
-    echo Error: npm is not installed or not found in PATH.
-    pause & exit /b 1
+    echo npm not found in PATH. Checking default installation directory... & echo.
+    if exist "%ProgramFiles%\nodejs\npm.cmd" (
+        set "npmPath=%ProgramFiles%\nodejs\npm.cmd"
+        echo npm found at default installation directory. & echo.
+    ) else if exist "%AppData%\npm\npm.cmd" (
+        set "npmPath=%AppData%\npm\npm.cmd"
+        echo npm found in AppData. & echo.
+    ) else (
+        echo Error: npm is not found.
+        echo. & echo Please run installation again.
+        pause & exit /b 1
+    )
 )
 
 if not exist "node_modules" (
-    echo Installing Node.js dependencies...
+    echo Installing Node.js dependencies... & echo.
     "%npmPath%" install
 
     if %errorlevel% neq 0 (
-        echo Error: Failed to install dependencies. Please check the npm setup and try again.
+        echo Error: Failed to install dependencies.
+        echo. & echo The repository may be temporarily unavailable.
+        echo. & echo Please run installation again.
         pause & exit /b 1
     )
 ) else (
-    echo Dependencies are already installed.
+    echo Dependencies are already installed. & echo.
 )
