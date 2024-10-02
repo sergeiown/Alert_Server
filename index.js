927246a00bdf7342dd2857b66a72a5e02e85f120
sergeiown <sergeiown@gmail.com>
2024-10-02 13:09:32 +0300

    Change the startup order

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ https://github.com/sergeiown/Alert_Server/blob/main/LICENSE */
 
 'use strict';
 
+const { handleExceptionAndRestart, logSystemEvents } = require('./modules/systemEventAndErrorHandler');
+const { createTrayIcon } = require('./modules/trayIconManager');
 const { fetchDataAndSaveToFile } = require('./modules/apiRequestHandler');
 const { showNotification } = require('./modules/alertNotifier');
-const { createTrayIcon } = require('./modules/trayIconManager');
-const { handleExceptionAndRestart, logSystemEvents } = require('./modules/systemEventAndErrorHandler');
 const { delayedCheckForUpdates } = require('./modules/updateHadler');
-
-fetchDataAndSaveToFile();
-
-showNotification();
-
-createTrayIcon();
 
 handleExceptionAndRestart();
 
 logSystemEvents();
+
+createTrayIcon();
+
+fetchDataAndSaveToFile();
+
+showNotification();
 
 delayedCheckForUpdates();
