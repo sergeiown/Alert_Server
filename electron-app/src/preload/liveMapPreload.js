6534ce4744d43921dede450643cd5621b208fa5d
sergeiown <sergeiown@gmail.com>
2026-08-22 16:33:31 +0300

    Add Dnipro river, region labels, and a layer toggle control to live map

--- electron-app/src/preload/liveMapPreload.js
+++ electron-app/src/preload/liveMapPreload.js
@@ -2,6 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('alertServerLiveMap', {
     getStrings: () => ipcRenderer.invoke('i18n:getStrings'),
+    getSettings: () => ipcRenderer.invoke('settings:get'),
     getBaseMapUrl: () => ipcRenderer.invoke('liveMap:getBaseMapUrl'),
     openExternal: (url) => ipcRenderer.invoke('system:openExternal', url),
 });
